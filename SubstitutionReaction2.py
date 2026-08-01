dff8a891f211ccfe905a7bdca7bf358d9dc7c893
zqh260619 <xb200808192023@outlook.com>
2026-08-02 02:36:57 +0800

    添加了芳香亲电取代反应的标题

--- SubstitutionReaction2.py
+++ SubstitutionReaction2.py
@@ -7,3 +7,11 @@ class test(Scene):
 
         title=Title(text=r"\text{一些常见的取代反应机理}\quad\text{续}",pos=ORIGIN)
         self.play(Write(title))
+        self.wait(1.5)
+        self.play(FadeOut(title))
+
+        #-----------------------SEAr reaction-----------------------
+
+        SEAr_reaction=Title(text=r"\mathrm{S_EAr}\text{（芳香亲电取代）}")
+        self.play(Write(SEAr_reaction))
+        self.wait(2)
