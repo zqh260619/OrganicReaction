30154b0469da9e172138a7a8bc758f24db729a23
zqh260619 <xb200808192023@outlook.com>
2026-08-03 00:19:30 +0800

    新增了包含离去基团的苯环结构的构建与动画展示代码，并通过动画展示。

--- SubstitutionReaction2.py
+++ SubstitutionReaction2.py
@@ -15,3 +15,18 @@ class test(Scene):
         SEAr_reaction=Title(text=r"\mathrm{S_EAr}\text{（芳香亲电取代）}")
         self.play(Write(SEAr_reaction))
         self.wait(2)
+
+        benzene=StructuralFormula(name="C1",pos=[0,0.5*bond_length,0],text=None)
+        benzene.add_atom(name="C2",direction=-30*DEGREES,text=None,bond_type=BondType.NORMAL_BOND,adjacency="C1")
+        benzene.add_atom(name="C3",direction=-90*DEGREES,text=None,bond_type=BondType.DOUBLE_BOND,adjacency="C2",
+                         side=-1,start_side_edge=True,end_side_edge=True)
+        benzene.add_atom(name="C4",direction=-150*DEGREES,text=None,bond_type=BondType.NORMAL_BOND,adjacency="C3")
+        benzene.add_atom(name="C5",direction=150*DEGREES,text=None,bond_type=BondType.DOUBLE_BOND,adjacency="C4",
+                         side=-1,start_side_edge=True,end_side_edge=True)
+        benzene.add_atom(name="C6",direction=90*DEGREES,text=None,bond_type=BondType.NORMAL_BOND,adjacency="C5")
+        benzene.add_bond(start="C6",end="C1",bond_type=BondType.DOUBLE_BOND,side=-1,start_side_edge=True,end_side_edge=True)
+        benzene.add_atom(name="E",direction=90*DEGREES,text="E",bond_type=BondType.NORMAL_BOND,adjacency="C1")
+
+        self.play(Create(benzene))
+
+        self.wait(2)
